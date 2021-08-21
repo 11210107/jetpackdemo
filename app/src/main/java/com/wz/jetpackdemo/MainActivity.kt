af75d6c207aefb66d422c131a421b4d68e33c8cc
wangzhen <zhen.wang@yintech.cn>
2021-08-21 17:36:08 +0800

    [navigation]配合bottomnavigationview使用。

--- app/src/main/java/com/wz/jetpackdemo/MainActivity.kt
+++ app/src/main/java/com/wz/jetpackdemo/MainActivity.kt
@@ -2,6 +2,12 @@ package com.wz.jetpackdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.wz.jetpackdemo.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +20,27 @@ class MainActivity : AppCompatActivity() {
 //                    .replace(R.id.container, MainFragment.newInstance())
 //                    .commitNow()
 //        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        setUpBottom(navHostFragment.navController)
     }
+
+    private fun setUpBottom(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setupWithNavController(navController)
+        bottomNav.setOnNavigationItemSelectedListener {item: MenuItem ->
+            val options = NavOptions.Builder()
+                .setPopUpTo(navController.currentDestination!!.id,true)
+                .setLaunchSingleTop(true)
+                .build()
+            try {
+                navController.navigate(item.itemId,null,options)
+                return@setOnNavigationItemSelectedListener true
+            } catch (e: Exception) {
+                return@setOnNavigationItemSelectedListener  false
+            }
+        }
+    }
+
+
 }
