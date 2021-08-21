8c9bdee817f2a0d1b6b462aed005730d1bed3a4b
wangzhen <zhen.wang@yintech.cn>
2021-08-21 16:36:03 +0800

    [navigation]navigation功能学习。

--- app/src/main/java/com/wz/jetpackdemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/wz/jetpackdemo/ui/main/MainFragment.kt
@@ -2,13 +2,18 @@ package com.wz.jetpackdemo.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import com.wz.jetpackdemo.R
 
 class MainFragment : Fragment() {
+    val args: MainFragmentArgs by navArgs()
 
     companion object {
         fun newInstance() = MainFragment()
@@ -21,6 +26,21 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.message).setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("key", "value")
+            Navigation.findNavController(it)
+                .navigate(R.id.action_fragment_main_to_fragment_login2, bundle)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("MainFragment:", "token:${args.token} id:${args.id}")
+    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
---
+++ app/src/main/java/com/wz/jetpackdemo/ui/user/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.wz.jetpackdemo.ui.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.wz.jetpackdemo.R
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.login_fragment, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.tv_login_register).setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_fragment_login_to_fragment_register2)
+        }
+
+        view.findViewById<TextView>(R.id.tv_login_main).setOnClickListener {
+            val action =
+                LoginFragmentDirections.actionFragmentLoginToFragmentMain2("token", 10001)
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
